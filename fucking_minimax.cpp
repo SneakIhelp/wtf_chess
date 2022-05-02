0220dd7fd2deedea0444e8d1a9092b01b4e27d06
Your Name <you@example.com>
2022-05-02 21:39:23 +0300

    i_ am_ fucking_shit_might_die_tonight

--- fucking_minimax.cpp
+++ fucking_minimax.cpp
@@ -83,71 +83,79 @@ int stoimost_figuri(string str) // ценность фигуры
         return 9;
 }
 
-int checkSlon(vector<vector<string>> matrix, int posV, int posG){
+long checkSlon(vector<vector<string>> matrix, int posV, int posG, string whiteOrDark){
     long lose = 0;
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
-            if (abs(i - posV) == abs(j - posG) and matrix[i][j].substr(1, 1) == 'B' and (i != posV and j != posG)) lose -= 1;
+            if (abs(i - posV) == abs(j - posG) and matrix[i][j].substr(1, 1) == whiteOrDark + 'B' and (i != posV or j != posG)) lose -= 1;
         } 
     }
     return lose;
 }
 
-int checkKnight(vector<vector<string>> matrix, int posV, int posG){
+long checkKnight(vector<vector<string>> matrix, int posV, int posG, string whiteOrDark){
     long lose = 0;
-    if(posV + 1 < 8 and posG + 2 < 8 and matrix[posV + 1][posG + 2].substr(1, 1) == 'N')   lose -= 1;
-    if(posV - 1 >= 0 and posG + 2 < 8 and matrix[posV - 1][posG + 2].substr(1, 1) == "N")  lose -= 1;
-    if(posV + 2 < 8 and posG + 1 < 8 and matrix[posV + 2][posG + 1].substr(1, 1) == 'N')   lose -= 1;
-    if(posV + 2 < 8 and posG - 1 >= 0 and matrix[posV + 2][posG - 1].substr(1, 1) == 'N')  lose -= 1;
-    if(posV - 1 >= 0 and posG - 2 >= 0 and matrix[posV - 1][posG - 2].substr(1, 1) == 'N') lose -= 1;
-    if(posV + 1 < 8 and posG - 2 >= 0 and matrix[posV + 1][posG - 2].substr(1, 1) == 'N')  lose -= 1;
-    if(posV - 2 >= 0 and posG - 1 >= 0 and matrix[posV - 2][posG - 1].substr(1, 1) == 'N') lose -= 1;
-    if(posV - 2 >= 0 and posG + 1 < 8  and matrix[posV - 2][posG + 1].substr(1, 1) == 'N') lose -= 1;
+    if(posV + 1 < 8 and posG + 2 < 8 and matrix[posV + 1][posG + 2].substr(1, 1) == whiteOrDark + 'N')   lose -= 1;
+    if(posV - 1 >= 0 and posG + 2 < 8 and matrix[posV - 1][posG + 2].substr(1, 1) == whiteOrDark + 'N')  lose -= 1;
+    if(posV + 2 < 8 and posG + 1 < 8 and matrix[posV + 2][posG + 1].substr(1, 1) == whiteOrDark + 'N')   lose -= 1;
+    if(posV + 2 < 8 and posG - 1 >= 0 and matrix[posV + 2][posG - 1].substr(1, 1) == whiteOrDark + 'N')  lose -= 1;
+    if(posV - 1 >= 0 and posG - 2 >= 0 and matrix[posV - 1][posG - 2].substr(1, 1) == whiteOrDark + 'N') lose -= 1;
+    if(posV + 1 < 8 and posG - 2 >= 0 and matrix[posV + 1][posG - 2].substr(1, 1) == whiteOrDark + 'N')  lose -= 1;
+    if(posV - 2 >= 0 and posG - 1 >= 0 and matrix[posV - 2][posG - 1].substr(1, 1) == whiteOrDark + 'N') lose -= 1;
+    if(posV - 2 >= 0 and posG + 1 < 8  and matrix[posV - 2][posG + 1].substr(1, 1) == whiteOrDark + 'N') lose -= 1;
 }
 
-int checkRook(vector<vector<string>> matrix, int posV, int posG){
+long checkRook(vector<vector<string>> matrix, int posV, int posG, string whiteOrDark){
     long lose = 0;
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
-            if ((posV == j or posG == i) and (posV != i or posG != j) and matrix[i][j].substr(1, 1) == 'R') lose -= 1;
+            if ((posV == j or posG == i) and (posV != i or posG != j) and matrix[i][j].substr(1, 1) == whiteOrDark + 'R') lose -= 1;
         }
     }
     return lose;
 }
 
-int checkKing(vector<vector<string>> matrix, int posV, int posG){
-    if(posV + 1 < 8 and posG - 1 >= 0 and matrix[posV + 1][posG - 1].substr(1, 1) == 'K') lose -= 1;
+long checkKing(vector<vector<string>> matrix, int posV, int posG, string whiteOrDark){
+    if(posV + 1 < 8 and posG - 1 >= 0 and matrix[posV + 1][posG - 1].substr(1, 1) == whiteOrDark + 'K') lose -= 1;
     if(posV + 1 < 8 and matrix[posV + 1][posG].substr(1, 1) == 'K') lose -= 1;
-    if(posV + 1 < 8 and posG + 1 < 8 and matrix[posV + 1][posG + 1].substr(1, 1) == 'K') lose -= 1;
-    if(posG - 1 < 8 and matrix[posV][posG - 1].substr(1, 1) == 'K') lose -= 1;
-    if(posG + 1 < 8 and matrix[posV][posG + 1].substr(1, 1) == 'K') lose -= 1;
-    if(posV - 1 >= 0 and posG - 1 >= 0 and matrix[posV - 1][posG - 1].substr(1, 1) == 'K') lose -= 1;
-    if(posV - 1 >= 0 and matrix[posV - 1][posG].substr(1, 1) == 'K') lose -= 1;
-    if(posV - 1 >= 0 and posG + 1 < 8 and matrix[posV - 1][posG + 1].substr(1, 1) == 'K') lose -= 1;
+    if(posV + 1 < 8 and posG + 1 < 8 and matrix[posV + 1][posG + 1].substr(1, 1) == whiteOrDark + 'K') lose -= 1;
+    if(posG - 1 < 8 and matrix[posV][posG - 1].substr(1, 1) == whiteOrDark + 'K') lose -= 1;
+    if(posG + 1 < 8 and matrix[posV][posG + 1].substr(1, 1) == whiteOrDark + 'K') lose -= 1;
+    if(posV - 1 >= 0 and posG - 1 >= 0 and matrix[posV - 1][posG - 1].substr(1, 1) == whiteOrDark + 'K') lose -= 1;
+    if(posV - 1 >= 0 and matrix[posV - 1][posG].substr(1, 1) == whiteOrDark + 'K') lose -= 1;
+    if(posV - 1 >= 0 and posG + 1 < 8 and matrix[posV - 1][posG + 1].substr(1, 1) == whiteOrDark + 'K') lose -= 1;
 
     return lose;
 }
 
-int checkFerz(vector<vector<string>> matrix, int posV, int posG){
+long checkFerz(vector<vector<string>> matrix, int posV, int posG, string whiteOrDark){
     long lose = 0;
     for(int i = 0; i < 8; i++){
         for(int j = 0; j < 8; j++){
-            if((j==posG||i==posV||j-posG==i-posV||posG-j==posV-i||-(j-posG)==i-posV||-(posG-j)==posV-i) and (posV != i or posG != j) and matrix[i][j].substr(1, 1) == 'Q') lose -= 1;
+            if((j==posG||i==posV||j-posG==i-posV||posG-j==posV-i||-(j-posG)==i-posV||-(posG-j)==posV-i) and (posV != i or posG != j) and matrix[i][j].substr(1, 1) == whiteOrDark + 'Q') lose -= 1;
         }
     }
     return lose;
 }
 
-int potery(vector<vector<string>> matrix, int posV, int posG){
+long potery(vector<vector<string>> matrix, int posV, int posG, long numb){
+    string whiteOrDark;
+    if(num % 2 == 1) whiteOrDark = "W";
+    else whiteOrDark = "D";
     long lose = 0;
-    if(posV + 1 < 8 and posG - 1 >= 0 and matrix[posV + 1][posG - 1].substr(1, 1) == 'P') lose -= 1; //пешка
-    if(posV + 1 < 8 and posG + 1 < 8 and matrix[posV + 1][posG + 1].substr(1, 1) == 'P') lose -= 1; //пешка
+    if(posV + 1 < 8 and posG - 1 >= 0 and matrix[posV + 1][posG - 1].substr(1, 1) == whiteOrDark + 'P') lose -= 1; //пешка
+    if(posV + 1 < 8 and posG + 1 < 8 and matrix[posV + 1][posG + 1].substr(1, 1) == whiteOrDark + 'P') lose -= 1; //пешка
     lose -= checkSlon(matrix, posV, posG);
     lose -= checkKnight(matrix, posV, posG);
     lose -= checkRock(matrix, posV, posG);
     lose -= checkKing(matrix, posV, posG);
     lose -= checkFerz(matrix, posV, posG);
 }
+
+long poluch(vector<vector<string>> matrix, int posV, int posG){
+    
+}
+
 
 int main()
 {
@@ -156,7 +164,7 @@ int main()
     string str,figura="",posicia_bukva="",posicia_4islo="",bar;
     int numb,pos,prov,prov2,color=0;
     vector<string> hodi;
-    vector<int> figuri {8,2,2,2,1,1};
+    vector<int> figuri {8,2,2,2,1,1}; // 0 - пешка, 1 - конь, 2 - слон, 3 - ладья, 4 - ферзь, 5 - король
     vector<vector<string>> matrix(8,vector<string>(8,".."));
     vector<vector<string>> dumai(8,vector<string>(8,".."));
 
@@ -217,13 +225,21 @@ int main()
                 figuri[4]--;
             else if(hodi[i].substr(0,1) == "K")
                 figuri[5]--;
-        }      
+        }     
+
+    long figuraWithMinPrice = -1;
+    for(int i = 0; i < long(figuri.size()); i++){
+        if(figuri[i] != 0) {
+            figuraWithMinPrice = i;
+            break;
+        }
+    } 
 
     int maximumPoluch = 0, maximumPoter = 0;
 
     for(int i = 0; i < 8; i++){         //сравнение потерь (potery) (т.е. получение очков противником) и получения очков нами 
         for(int j = 0; j < 8; j++){
-            
+            cout << potery(matrix, i, j) << endl;
         }
     }
 }
